02ca3448486e5f452ee9a95a225a0c09c327998b
klpod221 <klpod221@gmail.com>
2023-10-13 16:31:58 +0700

    update router

---
+++ src/pages/Expense.vue
@@ -0,0 +1,14 @@
+<template>
+  <my-card>
+    <div class="flex flex-col items-center justify-center h-full">
+      <font-awesome-icon icon="tools" class="text-6xl text-gray-500" />
+      <div class="text-2xl text-gray-500 mt-4">Under Construction</div>
+    </div>
+  </my-card>
+</template>
+
+<script>
+export default {
+  name: 'ExpensePage',
+};
+</script>
